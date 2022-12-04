48b3dfde32c85af9a96654b4b65d7034672f1291
exzerth <osinayaoludare@gmail.com>
2022-12-04 02:08:56 +0100

    updating components

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -1,19 +1,34 @@
 import Image from 'next/image'
 import mobileBanner from '../images/mobile-home-banner.jpeg'
+import desktopBanner from '../images/desktop-banner.jpeg'
 import '../styles/HomeBanner.module.css'
 
 const HomeBanner = () => {
   return (
     <>
-      <div className="w-full">
+      <div className="w-full md:px-[24px] mt-[16px]">
         <Image
           src={mobileBanner}
-          className="object-cover w-full"
+          className="object-cover w-full md:hidden"
           alt=""
         />
-        <div className="absolute inset-x-0 bottom-10 text-center">
-            <h1 className='text-2xl text-[#0f4c8d] font-bold my-2.5'>Get Ready Before the Rush</h1>
-            <a className='bg-[#ff9b00] text-white font-bold text-lg p-[15px] min-w-[160px] min-h-[48px] rounded inline-block' href="#shop">SHOP NOW</a>
+
+        <Image
+          src={desktopBanner}
+          className="hidden md:block h-[256px]"
+          alt=""
+        />
+
+        <div className="absolute inset-x-0 bottom-10 text-center md:text-start md:top-[65px] md:pl-[74px]">
+          <h1 className="text-2xl text-[#0f4c8d] font-bold my-2.5">
+            Get Ready Before the Rush
+          </h1>
+          <a
+            className="bg-[#ff9b00] text-white font-bold text-lg p-[15px] md:py-[12px] min-w-[160px] min-h-[48px] rounded inline-block md:text-center"
+            href="#shop"
+          >
+            SHOP NOW
+          </a>
         </div>
       </div>
     </>
--- components/ProductsCard.jsx
+++ components/ProductsCard.jsx
@@ -7,7 +7,7 @@ const ProductsCard = () => {
   return (
     <>
         <div className={cardStyles.card}>
-            <Link href="/banners" className={cardStyles.link}>
+            <Link href="#products" className={cardStyles.link}>
                 <div className="">
                     <div className="min-h-[144px] w-full">
                         <Image src={holidayCard} className="m-auto" alt=""/>
