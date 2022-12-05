30144ceb26b14141404174eafafa7afe33a677fa
exzerth <osinayaoludare@gmail.com>
2022-12-05 04:11:24 +0100

    fixing banners

--- components/BannerProducts.jsx
+++ components/BannerProducts.jsx
@@ -5,10 +5,11 @@ import categories from "../utils/categories.json"
     const productTypeName = props.productTypeName
 
    return(   
-      <div className="mt-5">
-        <div className="w-full flex justify-between lg:justify-center px-[15px] flex-wrap">      
+      <div className="container mt-5">
+        <div className="w-full flex justify-center px-[15px] md:px-[24px] flex-wrap">
+          <div className="columns-2 lg:columns-3 w-full">
           {categories.banners.map((banner) => (
-            <div key={banner.name} className="m-2 w-[180px] md:w-[348px] lg:w-[310px] min-h-max border">
+            <div key={banner.name} className="w-full mb-[20px] border lg:inline-block min-h-[320px]">
               <img src={banner.image} alt={banner.name + 'Image'} />
               <h1 className="text-xl text-primary pt-4 font-semibold text-[15px] pl-[8px] pb-[8px] md:text-[22px] md:text-center">
                 {productTypeName}
@@ -18,11 +19,12 @@ import categories from "../utils/categories.json"
                   <li className="mb-[5px]" key={index}>{detail}</li>
                 ))}
               </ul>
-              <div className="pr-[24px] pb-[24px] flex justify-end">
+              <div className="hidden md:flex pr-[24px] pb-[24px] justify-end">
                 <button className="hidden md:block min-w-[160px] rounded bg-[#FF9B00] text-white p-[15px] text-[18px] font-bold mt-[16px]">SHOP NOW</button>
               </div>
             </div>
           ))}
+          </div>
         </div> 
       </div>
     )
