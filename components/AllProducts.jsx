5c9f6d176d4a975ef0f8444945812bec4c018288
exzerth <osinayaoludare@gmail.com>
2022-12-13 12:01:55 +0100

    all products page

---
+++ components/AllProducts.jsx
@@ -0,0 +1,154 @@
+import Link from "next/link"
+
+const AllProducts = () => {
+  return (
+    <>
+    <div className="products px-[15px]">
+        <div className="title text-[24px] mt-[20px] mb-[10px] text-[#0f4c8d] font-bold">Product List</div>
+        <div className="grid grid-cols-2 gap-4">
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Business Marketing</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Booklets</Link></li>
+                    <li><Link href="#">Bookmarks</Link></li>
+                    <li><Link href="#">Brochures</Link></li>
+                    <li><Link href="#">Business Flyers</Link></li>
+                    <li><Link href="#">Catalogs</Link></li>
+                    <li><Link href="#">Circle Business Cards</Link></li>
+                    <li><Link href="#">Door Hangers</Link></li>
+                    <li><Link href="#">Foil Bookmarks</Link></li>
+                    <li><Link href="#">Foil Business Cards</Link></li>
+                    <li><Link href="#">Gift Certificates</Link></li>
+                    <li><Link href="#">Magazines</Link></li>
+                    <li><Link href="#">Magnets</Link></li>
+                    <li><Link href="#">Metallic Bookmarks</Link></li>
+                    <li><Link href="#">Metallic Business Cards</Link></li>
+                    <li><Link href="#">Metallic Postcards</Link></li>
+                    <li><Link href="#">Metallic Rack Cards</Link></li>
+                    <li><Link href="#">Metallic Sales/Data Sheets</Link></li>
+                    <li><Link href="#">Multi-Page</Link></li>
+                    <li><Link href="#">Newsletters</Link></li>
+                    <li><Link href="#">Painted Edge Business Cards</Link></li>
+                    <li><Link href="#">Postcards</Link></li>
+                    <li><Link href="#">Presentation Folders</Link></li>
+                    <li><Link href="#">Rack Cards</Link></li>
+                    <li><Link href="#">Raised Spot UV Business Cards</Link></li>
+                    <li><Link href="#">Raised Spot UV Postcards</Link></li>
+                    <li><Link href="#">Rip Cards</Link></li>
+                    <li><Link href="#">Sales/Data Sheets</Link></li>
+                    <li><Link href="#">Silk Business Cards</Link></li>
+                    <li><Link href="#">Silk Postcards</Link></li>
+                    <li><Link href="#">Silk Presentation Folders</Link></li>
+                    <li><Link href="#">Spot UV Business Cards</Link></li>
+                    <li><Link href="#">Spot UV Postcards</Link></li>
+                    <li><Link href="#">Spot UV Rack Cards</Link></li>
+                    <li><Link href="#">Velvel Business Cards</Link></li>
+                    <li><Link href="#">Velvel Postcards</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Event Marketing</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Backdrops</Link></li>
+                    <li><Link href="#">Bulk Posters</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+            <div className="gridcontent">
+                <div className="contenttitle font-bold text-[16px] my-[10px] text-[#0f4c8d]">Apparels</div>
+                <ul className="text-[14px] mb-[24px] font-normal text-primary">
+                    <li><Link href="#">Hats</Link></li>
+                    <li><Link href="#">Jackets</Link></li>
+                    <li><Link href="#">Polo Shirts</Link></li>
+                    <li><Link href="#">Sweatshirt</Link></li>
+                    <li><Link href="#">T Shirts</Link></li>
+                    <li><Link href="#">Work Shirts</Link></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default AllProducts
